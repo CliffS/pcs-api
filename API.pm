62c187faa80a5707370d640fb385e541fe84011a
CliffS <cliff@may.be>
2013-07-29 23:12:56 +0100

    Still testing pcs.  qd sort of started.

--- API.pm
+++ API.pm
@@ -10,26 +10,14 @@ use JSON::XS;
 use LWP;
 use URI::Escape;
 use Hash::Case::Lower;
+use MIME::Base64 qw(encode_base64url encode_base64);
+use Fcntl;
+use Encode;
 
 use Data::Dumper;
 use Carp;
 
 use constant URI => 'https://www.pcs-isaac.co.uk/wsPCS.asmx/';
-
-sub getstring(%)
-{
-    local $_;
-    tie my %params, 'Hash::Case::Lower';
-    %params = @_;
-    my $string = '';
-    foreach (keys %params)
-    {
-	(my $key = $_) =~ s/^./\u$&/;
-	$key =~ s/(?<=_)\w/\u$&/g;
-	$string .= '&' . uri_escape($key) . '=' . uri_escape($params{$_});
-    }
-    return $string;
-}
 
 
 sub new
@@ -45,6 +33,23 @@ sub new
 }
 
 sub DESTROY { }
+
+sub getstring
+{
+    my $self = shift;
+    local $_;
+    tie my %params, 'Hash::Case::Lower';
+    %params = @_;
+    my $string = '';
+    foreach (keys %params)
+    {
+	(my $key = $_) =~ s/^./\u$&/;
+	$key =~ s/(?<=_)\w/\u$&/g;
+	$string .= '&' . uri_escape($key) . '=' . uri_escape($params{$_});
+    }
+    $string = "?APIToken=$self->{apitoken}$string";
+    return $string;
+}
 
 sub AUTOLOAD
 {
@@ -71,13 +76,13 @@ sub AUTOLOAD
 	$optional{Appointment_Alternate_Address} = 'No';
 	%params = ( %optional, %params );
     }
-    my $string = getstring %params;
-    my $url = URI . "$name?APIToken=$self->{apitoken}$string";
-    say $url;
+    my $string = $self->getstring(%params);
+    my $url = URI . "$name$string";
+    #say $url;
     my $ua = new LWP::UserAgent;
     my $response = $ua->get($url);
-    say '---  ', $response->code, " ", $response->message;
-    croak $response->content unless $response->is_success;
+    #say '---  ', $response->code, " ", $response->message;
+    croak $response->decoded_content unless $response->is_success;
     my $xml = new XML::Simple;
     my $content = $xml->XMLin($response->decoded_content)->{content};
     my $json = new JSON::XS;
@@ -89,13 +94,71 @@ sub download_paperwork
 {
     my $self = shift;
     my %params = @_;
-    my $string = getstring %params;
-    my $url = URI . "Download_Paperwork?APIToken=$self->{apitoken}$string";
-    say $url;
+    my $string = $self->getstring(%params);
+    my $url = URI . "Download_Paperwork$string";
+    #say $url;
     my $ua = new LWP::UserAgent;
     my $response = $ua->get($url);
-    say $response->code, " ", $response->message;
-    return $response->content;
+    croak $response->decoded_content unless $response->is_success;
+    return {
+	filename    => $response->filename,
+	file	    => $response->content,
+    };
+}
+
+sub upload_case_paperwork
+{
+    my $self = shift;
+    my %params = @_;
+    my $filename = $params{filename};
+    my $file;
+    {
+	local $/;
+	open my $hand, $filename or croak "Open $filename: $!";
+	$file = <$hand>;
+	close $hand;
+    }
+    my $string = $self->getstring(%params);
+    my $url = URI . "Upload_Case_Paperwork";
+    #my $url = URI . "Upload_Case_Paperwork$string";
+    say $url;
+    $params{fileByte} = encode_base64($file, '');
+    $string = $self->getstring(%params);
+    my $ua = new LWP::UserAgent;
+    #my $response = $ua->get($url);
+    #return $response->decoded_content;
+    my $response = $ua->post($url,
+	Content => substr $string, 1
+#	[
+#	    APIToken => $self->{apitoken},
+#	    Case_Number => $params{case_number},
+#	    fileByte => encode_base64($file, ''),
+#	    Filename => $filename,
+#	],
+##	Filename => $filename,
+    );
+    say Dumper $response; exit;
+    say '---  ', $response->code, " ", $response->message;
+    return $response->decoded_content;
+}
+
+sub upload_paperwork
+{
+    my $self = shift;
+    my $filename = shift;
+    #my $url = URI . 'upload-paperwork.aspx';
+    my $url = 'https://www.pcs-isaac.co.uk/upload-paperwork.aspx';
+    my $ua = new LWP::UserAgent;
+    my $response = $ua->post($url,
+	[
+	    fileByte => [$filename],
+	    FileName => $filename,
+	    Content_Type => 'application/pdf',
+	],
+	Content_Type => 'form-data',
+    );
+    print $response->request->headers->as_string, "\n", $response->request->content;
+    return $response->decoded_content;
 }
 
 1;
