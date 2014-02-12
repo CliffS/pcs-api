67f390abf5a51e21165384798e880af74a8730b8
CliffS <cliff@may.be>
2014-02-12 10:17:07 +0000

    PCS::Case to handle the return value of search.

---
+++ Case.pm
@@ -0,0 +1,44 @@
+package PCS::Case;
+
+use strict;
+use warnings;
+use 5.14.0;
+use utf8;
+
+use DateTime;
+use DateTime::Format::Strptime;
+
+sub new
+{
+    my $class = shift;
+    my $result = shift;
+    my $strp = new DateTime::Format::Strptime(
+	pattern	    => '%d/%m/%Y %H:%M:%S',
+	locale	    => 'en_GB',
+	timezone    => #Europe/London',
+	on_error    => 'croak',
+    );
+    my $self = {
+	id	    => $result->{CaseNo},
+	name	    => $result->{CustomerName},
+	status	    => $result->{Status},
+	reference   => $result->{Reference},
+	postcode    => $result->{Postcode},
+	instructor  => $result->{Instructor},
+	appointment => $strp->parse_datetime($result->{ApptDate}),
+	instructed  => $strp->parse_datetime($result->{InstructionDate}),
+    };
+    bless $self, $class;
+}
+
+sub AUTOLOAD
+{
+    my $self = shift;
+    (my $field = our $AUTOLOAD) =~ s/.*:://;
+    croak "Unknown field: $field" unless exists $self->{$field};
+    return $self->{$field};
+}
+
+sub DESTROY {}
+
+1;
