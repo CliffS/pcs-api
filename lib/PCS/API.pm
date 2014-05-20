bed477724e8cb58f6e9f3bb5fb3738fde2f4a436
CliffS <cliff@may.be>
2014-05-20 23:48:39 +0100

    PCS::API documented.

--- lib/PCS/API.pm
+++ lib/PCS/API.pm
@@ -29,12 +29,48 @@ Version v1.0.0;
 
 our $VERSION = v1.0.0;   # Don't forget the pod above
 
+=head1 SYNOPSYS
+
+This is a full interface to the SOAP API for Private Client Solutions
+(PCS), a UK document collection and delivery service.
+
+    my $pcs = new PCS::API($apikey);
+    my @jobtypes = $pcs->jobtypes;  # returns a hash of name => id
+    my $jobid = $pcs->instruct(\%client, @files);
+
+=cut
+
 use enum qw{NONE CASE REF INSTRUCTOR NAME POSTCODE STATUS};
 
 sub byappointment
 {
     $a->appointment <=> $b->appointment;
 }
+
+=head1 CONSTRUCTOR
+
+=head2 new
+
+    my $pcs = new PCS::API($apikey);
+
+The only parameter is a string containing the secret API key as provided
+by PCS.
+
+=head1 GENERAL METHODS
+
+All the methods below act upon a PCS::API object constructed with
+L</new>.
+
+=head2 jobtypes
+
+This takes no parameters and returns a hash of the jobtypes in the
+form of C<jobtype => jobtype_id>.  The jobtype_id is used in the
+L</instruct> method below.
+
+    my %jobtypes = $pcs->jobtypes;
+    my $id = $jobtypes{MYTYPE};
+
+=cut
 
 sub jobtypes
 {
@@ -44,6 +80,86 @@ sub jobtypes
     my %jobs = map { $_->{JobTypeName} => $_->{JobTypeID}  } @$types;
     return %jobs;
 }
+
+=head2 instruct
+
+This routine instructs PCS to send a courier to a given name
+and address at a particular time.  It returns the job ID.
+
+    my $job_id = $pcs->instruct($client, @files);
+
+=head3 parameters
+
+=over
+
+=item $client
+
+This is a hashref containing the following keys:
+
+=over
+
+=item jobtype
+
+from above, required
+
+=item reference
+
+This is our reference, required
+
+=item title
+
+(e.g. Mr Mrs)
+
+=item first_name
+
+required
+
+=item surname
+
+required
+
+=item telephone
+
+=item mobile
+
+=item email
+
+=item address1
+
+required
+
+=item address2
+
+=item town
+
+required
+
+=item postcode
+
+formatted in the normal way, required
+
+=item appointment
+
+this is a DateTime object.  The day should be Monday to Saturday,
+the hour must be between 8 and 20 inclusinve and the minutes
+must be a multiple of five.  It must be no earlier than 3 working
+hours from the time L</instruct> is called.
+
+=back
+
+=item @files
+
+This is an array of filenames to upload to the case.  These are the
+files that the courier will print off to take to an appointment.
+
+=item return value
+
+The case ID is returned.  This is the key for many of the functions
+below.
+
+=back
+
+=cut
 
 # Returns ID
 sub instruct
@@ -94,6 +210,29 @@ sub instruct
     return $self->get_case($response[1]);
 }
 
+=head2 download_paperwork
+
+This only downloads the paperwrk that was uploaded by the
+courier, not the original uploaded paperwork.
+
+    $items = $pcs->download_paperwork($id, $path);
+
+=item $id
+
+This is the case ID returned by L</instruct>.
+
+=item $path
+
+This is the path to whicj the files should be downloaded.  
+
+=item return value
+
+This is the number of items downloaded.
+
+=back
+
+=cut
+
 # Returns the number of files downloaded
 sub download_paperwork
 {
@@ -117,6 +256,15 @@ sub download_paperwork
     return $count;
 }
 
+=head2 cancel
+
+    $pcs->cancel($id);
+
+This is used to cancel a case.  It is passed the case ID returned
+from L</instruct>.  There is no return value, the call croaks on error.
+
+=cut
+
 # No return value: croaks on error.
 sub cancel
 {
@@ -126,6 +274,19 @@ sub cancel
     $result = $result->{Cancel_AppointmentResult};
     croak $result->{Status} unless $result->{Status} eq 'OK';
 }
+
+=head1 SEARCH METHODS
+
+All the search methods take a similar format.  They all
+return eith a single case or an array of cases.
+Each case is an instance of L<PCS::Case>.  
+
+Where an array of L<PCS::Case> is returned, it will be in order
+of appointment time.  Most of the search functions can take an optional
+start and end. These are C<DateTime> objects limiting the earliest
+and optionally the latest appointment times for the cases returned.
+
+=cut
 
 sub search
 {
@@ -164,6 +325,15 @@ sub search
     return @cases;
 }
 
+=head2 get_case
+
+    my $case = $pcs->get_case($id);
+
+This returns a single L<PCS::Case> for the given ID or C<undef>
+if not found.
+
+=cut
+
 sub get_case
 {
     my $self = shift;
@@ -173,6 +343,15 @@ sub get_case
     return shift @cases;
 }
 
+=head2 get_by_ref
+
+    my @cases = $pcs->get_by_ref($reference);
+
+This returns an array of L<PCS::Case> as there is no restriction
+on duplicate references.
+
+=cut
+
 sub get_by_ref
 {
     my $self = shift;
@@ -180,6 +359,15 @@ sub get_by_ref
     my @cases = $self->search(REF, $ref, @_);
     return sort byappointment @cases;
 }
+
+=head2 get_by_postcode
+
+    my @cases = $pcs->get_by_postcode($postcode);
+
+This takes a formatted postcode and returns an array
+of L<PCS::Case>.
+
+=cut
 
 sub get_by_postcode
 {
@@ -189,6 +377,15 @@ sub get_by_postcode
     return sort byappointment @cases;
 }
 
+=head2 get_all_cases
+
+    my @cases = $pcs->get_all_cases([$start [,$end]]);
+
+This returns all cases, all cases after $start (a C<DateTime>)
+or all cases between $start and $end inclusive.
+
+=cut
+
 sub get_all_cases
 {
     my $self = shift;
@@ -196,11 +393,30 @@ sub get_all_cases
     return sort byappointment @cases;
 }
 
+=head2 get_by_status
+
+    my @cases = $pcs->get_by_status($status [,$start [, $end]]);
+
+Gets all the cases in range with a particular status.  See
+L</PCS STATUSES> below for a full list of statuses.
+
+=cut
+
 sub get_by_status
 {
     my $self = shift;
     return $self->search(STATUS, @_);
 }
+
+=head2 get_complete
+
+    my @cases = $pcs->get_complete([$start [, $end]]);
+
+Gets all cases in range that are complete.  Complete means that
+the paperwork has been returned and a tracking number is
+available.
+
+=cut
 
 sub get_complete
 {
@@ -208,6 +424,14 @@ sub get_complete
     my @cases = $self->get_by_status('COMPLETE', @_);
     return @cases;
 }
+
+=head2 get_pending
+
+    my @cases = $pcs->get_pending([$start [,$end]]);
+
+Gets all pending cases (i.e. not complete, not signed and not failed).
+
+=cut
 
 sub get_pending
 {
@@ -220,6 +444,15 @@ sub get_pending
     return sort byappointment @cases;
 }
 
+=head2 get_signed
+
+    my @cases = $pcs->get_signed([$start [,$end]]);
+
+Gets all signed cases. This reurns all cases that have been successfully
+signed.  The paperwork may not yet be available.
+
+=cut
+
 sub get_signed
 {
     my $self = shift;
@@ -230,6 +463,14 @@ sub get_signed
     }
     return sort byappointment @cases;
 }
+
+=head2 get_failed
+
+    my @cases = $pcs->get_failed([$start [,$end]]);
+
+Gets all failed cases, irrespective of the reason.
+
+=cut
 
 sub get_failed
 {
@@ -244,6 +485,14 @@ sub get_failed
     return sort byappointment @cases;
 }
 
+=head2 get_by_jobtype
+
+    my @cases = $pcs->get_by_jobtype($jobtype_id [, $start, [, $end]]);
+
+Get all cases for a particular jobtype ID.
+
+=cut
+
 sub get_by_jobtype
 {
     my $self = shift;
@@ -252,5 +501,90 @@ sub get_by_jobtype
     @cases = grep { $_->jobtype == $jobtype } @cases;
     return @cases;
 }
+
+=head1 PCS STATUSES
+
+The following is a full list of the raw statuses provided by
+the PCS API.
+
+=over
+
+=item TBA
+
+To be allocated - Your appointment has been successfully added to our portal
+and we are in the process of allocating a field agent to attend.  ALLOCATED
+Your appointment has been allocated to one of our field agents and will be
+attended on the specified date and time.
+
+=item SIGNED
+
+Your appointment has been attened by our filed agent and the client has signed
+the documentation.  UPLOADED Our field agent has uploaded scanned copies of the
+documentation that was completed on the appointment.
+
+=item COMPLETE
+
+Our field agent has posted the documentation and has added a Royal Mail
+tracking number for your reference.
+
+=item CANX_AT
+
+The client was not in when our agent attended the appointment.
+
+=item CANX_B4<60
+
+The appointment has been cancelled less than 60 minutes prior to the
+appointment time.
+
+=item CANX_B4>60
+
+The appointment has been cancelled more than 60 minutes prior to the
+appointment time.
+
+=item REFUSED
+
+Our field agent attended your appointmetn but the customer was unwilling to
+sign or complete your documentation.
+
+=item PROBLEM
+
+This status is used by our internal team if they are struggling to find an
+agent to attend your appointment. You don't need to do anything, our internal
+team will contact you if we require any assistance
+
+=back
+
+=head1 EXPORTS
+
+Nothing.
+
+=head1 BUGS
+
+This is fairly untested code.  Expect bugs.
+
+Also the calls to L</cancel> and L</download_paperwork> should
+probably be calls on a single case.  Logically then, L</instruct>
+should return a L<PCS::Case> rather than a case ID.
+
+SEE ALSO
+
+L<PCS::Case> - and individual case returned by the search functions.
+
+=head1 AUTHOR INFORMATION
+
+Cliff Stanford C<< <cliff@may.be> >>
+
+=head1 SOURCE REPOSITORY
+
+The source is maintained at a public Github repository at
+L<https://github.com/CliffS/pcs-api>.
+
+=head1 LICENCE AND COPYWRITE
+
+Copyright © 2014, Cliff Stanford C<< <cliff@may.be> >>.
+All rights reserved.
+
+This module is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
 
 1;
